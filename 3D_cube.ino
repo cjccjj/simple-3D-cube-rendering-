5d7ba2190ae2ba41cfb6d851c443e275a8506d1e
1023 studio <110604948+cjccjj@users.noreply.github.com>
2023-03-17 07:36:22 +0800

    Update 3D_cube.ino

--- 3D_cube.ino
+++ 3D_cube.ino
@@ -39,23 +39,31 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
                          OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 //customization
-int cubesize = 30; //in pixels
+int cubesize = 15; //in pixels
 int fl = 999; //distance of cammera, 80 looks fine, set to >=999 for orthographic projection
-int angle = 0; //starting angle
-int zoom = 100; //zoom starting zoom
-int d = 1; //zoom in or out first
-int delay_mils = 0; //the system need about 35 to work for my hardware (max fps 30), this is an addtion to that, fps = 1000/(35+delay_mils)
-
-//for fps display
+int delay_mils = 5; //the system need about 35 to work for my hardware (max fps 30), this is an addtion to that, fps = 1000/(35+delay_mils)
+int zoom = 100;
+int d = 1; //zoom in or out 
+//for display fps
 long lastframe_mil = 0;
 long newframe_mil = 0;
 int fps = 0;
 
+//cube coordinate see top of page
+float vertex[8][3] = {
+  { -1, -1, 1},
+  {1, -1, 1},
+  { -1, 1, -1},
+  { -1, 1, 1},
+  {1, -1, -1},
+  { -1, -1, -1},
+  {1, 1, 1},
+  {1, 1, -1}
+};
 //2D vertex coord for drawing
-int vertex_2D[8][3];
+int vertex_2D[8][2];
 //which vertex is at back most
 int back_vertex = 0;
-
 
 //draw line linking  vertex a to vertex b
 void draw_line(int a, int b) {
@@ -70,92 +78,41 @@ void draw_frame(void) {
 
   for (int i = 0; i < 8; i += 2) {
     for (int j = i + 1; j < i + 7; j += 2) {
-      if (back_vertex != i && back_vertex != j%8 ) {
-        draw_line(i, j%8 );
+      if (back_vertex != i && back_vertex != j % 8 ) {
+        draw_line(i, j % 8 );
       }
     }
   }
 }
 
-void cubeloop(void) {
-
-  float rot, rotx, roty, rotz, rotxx, rotyy, rotzz;
-
-  //cube coordinate see top of code
-  int vertex[8][3] = {
-    { -1, -1, 1},
-    {1, -1, 1},
-    { -1, 1, -1},
-    { -1, 1, 1},
-    {1, -1, -1},
-    { -1, -1, -1},
-    {1, 1, 1},
-    {1, 1, -1}
-  };
-
-  //calculate vertex coord with size
+//rotate in axel ax, x:ax=0, y:ax=1, z:az=2
+//and then project to 2D
+void rotate(int ax, int rot_degree) {
+  float rot = rot_degree *  0.0174532; //0.0174532 = one degree
   for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 3; j++) {
-      vertex[i][j] = vertex[i][j] * cubesize / 2;
-    }
-  }
-
-  while (true) { //loop
-    //calculate new vertex coord
-    for (int i = 0; i < 8; i++) {
-      rot = angle * 0.0174532; //0.0174532 = one degree
-      //rotateY
-      rotz = vertex[i][2] * cos(rot) - vertex[i][0] * sin(rot);
-      rotx = vertex[i][2] * sin(rot) + vertex[i][0] * cos(rot);
-      roty = vertex[i][1];
-      //rotateX
-      rotyy = roty * cos(rot) - rotz * sin(rot);
-      rotzz = roty * sin(rot) + rotz * cos(rot);
-      rotxx = rotx;
-      //rotateZ
-      rotx = rotxx * cos(rot) - rotyy * sin(rot);
-      roty = rotxx * sin(rot) + rotyy * cos(rot);
-      rotz = rotzz;
-
-      //3D to 2D  projection
-      rotx = rotx * fl / (fl + rotz);
-      roty = roty * fl / (fl + rotz);
-
-      //zoom and move to display coord center
-      vertex_2D[i][0] = rotx * zoom / 100 + SCREEN_WIDTH / 2 ;
-      vertex_2D[i][1] = roty * zoom  / 100 + SCREEN_HEIGHT / 2;
-      vertex_2D[i][2] = rotz;
-
-      //look for which vertex is at back most
-      if (vertex_2D[back_vertex][2] < vertex_2D [i][2]) {
-        back_vertex = i;
-      }
-    }
-
-    display.clearDisplay();
-    draw_frame();
-
-    //show fps
-    display.setCursor(2, 2);
-    newframe_mil = millis();
-    fps = 1000 / (newframe_mil - lastframe_mil) ;
-    lastframe_mil = newframe_mil;
-    display.print("fps ");
-    display.print(fps);
-    display.display();
-    delay(delay_mils); //to reduce fps
-
-    angle = (angle + 3) % 360;  //rotate step at 3 degrees
-    zoom = zoom + 1 * d; //zooming step at 1%
-    if (zoom >= 200) { //zooming range 1.25 to 2.0
-      d = -1;
-    }
-    if (zoom <= 25) {
-      d = 1;
+    //rotate
+    float temp = vertex[i][(ax + 1) % 3] * cos(rot) - vertex[i][(ax + 2) % 3] * sin(rot);
+    vertex[i][(ax + 2) % 3] = vertex[i][(ax + 2) % 3] * cos(rot) + vertex[i][(ax + 1) % 3] * sin(rot);
+    vertex[i][(ax + 1) % 3] = temp;
+    //project
+    vertex_2D[i][0] = (vertex[i][0] * cubesize * fl / (vertex[i][2] * cubesize + fl)) * zoom / 100 + SCREEN_WIDTH / 2;
+    vertex_2D[i][1] = (vertex[i][1] * cubesize * fl / (vertex[i][2] * cubesize + fl)) * zoom / 100 + SCREEN_HEIGHT / 2;
+    //looking for back most vertex number
+    if (vertex[back_vertex][2] < vertex [i][2]) {
+      back_vertex = i;
     }
   }
 }
 
+void display_fps() {
+  //show fps
+  display.setCursor(2, 2);
+  newframe_mil = millis();
+  fps = 1000 / (newframe_mil - lastframe_mil) ;
+  lastframe_mil = newframe_mil;
+  display.print("fps ");
+  display.print(fps);
+}
 void setup() {
   Serial.begin(9600);
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
@@ -170,5 +127,25 @@ void setup() {
 }
 
 void loop() {
-  cubeloop();
+  while (true) { //loop
+    display.clearDisplay();
+    draw_frame();
+    display_fps();
+    display.display();
+    delay(delay_mils); //to reduce fps
+
+    //roate x,y,z each for 2 degree,
+    rotate(0, 2);
+    rotate(1, 2);
+    rotate(2, 2);
+
+    //zooming in and out
+    zoom = zoom + 1 * d;
+    if (zoom >= 150) {
+      d = -1;
+    }
+    if (zoom <= 25) {
+      d = 1;
+    }
+  }
 }
